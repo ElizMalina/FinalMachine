354d47cdddca0f0e3623e66f8aeca6c17e4b4cdb
Elizaveta Malina <elizavetamalina8975@gmail.com>
2025-10-17 12:49:58 +0300

    feat: добавление программы поиска цепочек этого языка в последовательности символов

--- FinalMachine.js
+++ FinalMachine.js
@@ -1,46 +1,39 @@
-
-let currentState='q1'; //начальное состояние
-
 const readline = require('readline');
 
-// Создаем интерфейс
+//начальное состояние
+let currentState='q1'; 
+
+//cоздаем интерфейс
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-//функция для проверки подцепочки 
-//изначально мы находимся в состоянии q1, читаем символ и идет по дка
-const tabl=new Map([
+//подпрограмма(распознающий КА)
+const KA=new Map([
         ['q1', (symbol) => {if(symbol=='~') return 'q2';
                             else if(symbol=="a") return '0';
                             else if(symbol=="b") return '0';
-                            return 'Error';
                             }],
         ['q2', (symbol) => {if(symbol=='~') return 'f';
                             else if(symbol=="a") return 'q3';
                             else if(symbol=="b") return '0';
-                            return 'Error';
                             }],
         ['q3', (symbol) => {if(symbol=='~') return '0';
                             else if(symbol=="a") return 'q4';
                             else if(symbol=="b") return 'q5';
-                            return 'Error';
                             }],
         ['q4', (symbol) => {if(symbol=='~') return '0';
                             else if(symbol=="a") return 'q3';
                             else if(symbol=="b") return 'q6';
-                            return 'Error';
                             }],
         ['q5', (symbol) => {if(symbol=='~') return '0';
                             else if(symbol=="a") return '0';
                             else if(symbol=="b") return 'q6';
-                            return 'Error';
                             }],
         ['q6', (symbol) => {if(symbol=='~') return 'f';
                             else if(symbol=="a") return 'q3';
                             else if(symbol=="b") return '0';
-                            return 'Error';
                             }],
         ['f', () => {return 'f';
                             }],
@@ -48,67 +41,71 @@ const tabl=new Map([
                             }]
 ]);
 
-rl.question('Введите строку: ', (input) => {
-  let count=1;
-  let num_index1=0;
-  let num_index2=0;
-  let itog_chain;
+//программа поиска цепочек этого языка в последовательности символов
+rl.question("Введите последоватеьность символов алфавита регулярного языка: ", (input) => {
+  let index=0;
 
+  while(index<input.length){
+    let count=1;
+    let index_first= undefined;
+    let index_last= undefined;
+    let itog_chain;
 
-  //ищем подцепочку
-  for(let i=0;i<input.length;++i){
-    const symbol=input[i];
+    //ищем цепочку
+    for(;index<input.length;++index){
+      const symbol=input[index];
 
-    //проверка на правильность ввода цепочки
-    if(symbol!='~' && symbol!='a' && symbol!='b'){
-      console.log("Неизвестный символ: ",symbol);
-      break;
-    }
-    
-    if(symbol==='~'){
-      if(count===1){
-        ++count;
-        num_index1=i;
-      }else if(count===2){
-        num_index2=i;
+      //проверка на правильность ввода цепочки
+      if(symbol!='~' && symbol!='a' && symbol!='b'){
+        console.log("Неизвестный символ: ",symbol);
         break;
       }
+      
+      if(symbol==='~'){
+        if(count===1){
+          ++count;
+          index_first=index;
+        }else if(count===2){
+          index_last=index;
+          break;
+        }
+      }
     }
-  }
 
-  //проверяем что нашли два ~~
-  if(num_index1!=undefined && num_index2!=undefined){
-    console.log(num_index1+1);
-    itog_chain=input.slice(num_index1,num_index2+1);
-  }else{
-    console.log("Не нашлось '~~' ");
-    rl.close();
-    return;
-  }
-
-  //проверка подцепочки 
-  for(let symbol of itog_chain){
-    //проверяем корректность работы функции
-    const transitionFunction=tabl.get(currentState);
-    if(typeof transitionFunction==='function'){
-      currentState = transitionFunction(symbol);
+    //проверяем что нашли префикс и суффикс
+    if(index_first!=undefined && index_last!=undefined){
+      itog_chain=input.slice(index_first,index_last+1);
     }else{
-      console.log("Нет функции перехода для состояния: ",currentState);
-      break;
+      rl.close();
+      return;
     }
 
-    //если мы попадаем в состояние '0', то просто переходим к следующему символу, так как подцепочка не подошла
-    if(currentState==='0'){
-      continue;
-    }
-    if(currentState==='Error'){
-      console.log("Ошибка в цепочке");
-    }
-    //цепочка успешно обработана
-    else if(currentState==='f'){
-      console.log(itog_chain);
-      break;
-    }
+    currentState='q1';
+
+    //проверка цепочки в КА
+    for(let symbol of itog_chain){
+      //проверяем корректность работы функции
+      const transitionFunction=KA.get(currentState);
+      if(typeof transitionFunction==='function'){
+        currentState = transitionFunction(symbol);
+      }else{
+        console.log("Нет функции перехода для состояния: ",currentState);
+        break;
+      }
+
+      //если мы попадаем в состояние '0', то переходим к следующему символу, так как подцепочка не подошла
+      if(currentState==='0'){
+        continue;
+      }
+      //цепочка успешно обработана
+      else if(currentState==='f'){
+        console.log(index_first+1+": ",itog_chain);
+        break;
+      }
+    } 
+
+    //далее начинаем рассматривать последовательность, начиная с суффикса обработанной цепочки
+    index=index_last; 
   }
 
   // Закрываем интерфейс
--- g/main.js
+++
@@ -1,4 +0,0 @@
-const {cin} = require('cpp-like-js')
-cin("Enter: ");
-const num = readInt();
-console.log(num);
