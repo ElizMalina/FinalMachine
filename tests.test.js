41e9f421dabd341c53bf483842db34a3f35fe70a
Elizaveta Malina <elizavetamalina8975@gmail.com>
2025-10-18 19:33:59 +0300

    feat: добавление комметариев в файл tests.test.js

--- tests.test.js
+++ tests.test.js
@@ -3,37 +3,37 @@ const { processInput } = require('./FinalMachine');
 describe('Тестирование', () =>{
   test('Пустая цепочка', () =>{
     const result=processInput('~~');
-    expect(result).toEqual([{start:1+': ',chain:'~~'}]);
+    expect(result).toEqual([{start:1+': ',chain:'~~'}]); //1: ~~
   });
 
   test('Простая цепочка №1', () =>{
     const result=processInput('~abb~');
-    expect(result).toEqual([{start:1+': ',chain:'~abb~'}]);
+    expect(result).toEqual([{start:1+': ',chain:'~abb~'}]); //1: ~abb~
   });
 
   test('Простая цепочка №2', () =>{
     const result=processInput('~abbaabaaabbaaaab~');
-    expect(result).toEqual([{start:1+': ',chain:'~abbaabaaabbaaaab~'}]);
+    expect(result).toEqual([{start:1+': ',chain:'~abbaabaaabbaaaab~'}]); //1: ~abbaabaaabbaaaab~
   });
 
   test('Простая цепочка №3',() =>{
     const result=processInput('~abb~ab~');
-    expect(result).toEqual([{start:1+': ',chain:'~abb~'}]);
+    expect(result).toEqual([{start:1+': ',chain:'~abb~'}]); //1: ~abb~
   });
 
   test('Две цепочки', () =>{
     const result=processInput('~abb~aaaab~');
-    expect(result).toEqual([{start:1+': ',chain:'~abb~'},
-                            {start:5+': ',chain:'~aaaab~'}
+    expect(result).toEqual([{start:1+': ',chain:'~abb~'},  //1: ~abb~
+                            {start:5+': ',chain:'~aaaab~'} //5: ~aaaab~
     ]);
   });
 
   test('Четыре цепочки', () =>{
     const result=processInput('~~abb~aaaab~abbaabaaabb~abb');
-    expect(result).toEqual([{start:1+': ',chain:'~~'},
-                            {start:2+': ',chain:'~abb~'},
-                            {start:6+': ',chain:'~aaaab~'},
-                            {start:12+': ',chain:'~abbaabaaabb~'}
+    expect(result).toEqual([{start:1+': ',chain:'~~'},             //1: ~~
+                            {start:2+': ',chain:'~abb~'},          //2: ~abb~
+                            {start:6+': ',chain:'~aaaab~'},        //6: ~aaaab~
+                            {start:12+': ',chain:'~abbaabaaabb~'}  //12: ~abbaabaaabb~
     ]);
   });
 
